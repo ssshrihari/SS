2145d6041afb86abb8e56d81a6d44c9f57cb8460
Gayathri <thiruvayipati.gayathri@capgemini.com>
2020-10-06 23:04:29 +0530

    latest app

--- root/app.js
+++ root/app.js
@@ -3,13 +3,13 @@ var MongoClient = require('mongodb').MongoClient;
 //Create a database named "mydb":
 var url = "mongodb+srv://dbUser:r5ZGxlkPdhohjDlI@gt.eef7b.mongodb.net/test";
 
-MongoClient.connect(ulr, function(err, db) {
+MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("test");
-  dbo.createCollection("customers", function(err, res) {
-    if (err) throw err;
-});
-var myobj = { name: "Company Inc", address: "Highway 37" };
+//   dbo.createCollection("customers", function(err, res) {
+//     if (err) throw err;
+// });
+var myobj = { name: "Company", address: "Highway 39" };
 
     dbo.collection("customers").insertOne(myobj, function(err, res) {
       if (err) throw err;
