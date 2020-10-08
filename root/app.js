ccec025ef72131dd972765b9f7079927b9c3fd27
shrihars <ssshrihari@gmail.com>
2020-10-08 11:46:04 +0530

    2

--- root/app.js
+++ root/app.js
@@ -3,19 +3,16 @@ var MongoClient = require('mongodb').MongoClient;
 //Create a database named "mydb":
 var url = "mongodb+srv://dbUser:r5ZGxlkPdhohjDlI@gt.eef7b.mongodb.net/test";
 
-MongoClient.connect(ulr, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("test");
-  dbo.createCollection("customers", function(err, res) {
+MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-});
-var myobj = { name: "Company Inc", address: "Highway 37" };
+    var dbo = db.db("test");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
 
     dbo.collection("customers").insertOne(myobj, function(err, res) {
       if (err) throw err;
       console.log("1 document inserted");
       db.close();
-    
+
 });
 });
 
