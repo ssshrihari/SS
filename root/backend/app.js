bfc9655bc410f56454b50a260828fecdbf548508
shrihars <ssshrihari@gmail.com>
2020-10-08 20:23:01 +0530

    final milestone 1

--- root/backend/app.js
+++ root/backend/app.js
@@ -7,24 +7,26 @@ const cors = require('cors');
 const app = express();
 app.use(cors())
 app.post('/click', (req, res) => {
-  MongoClient.connect(urll, function (err, db) {
+  const requestBody = [];
+  var myobj;
+  req.on('data', (chunks) => {
+    requestBody.push(chunks);
+  });
+  req.on('end', () => {
+    var parsedData = Buffer.concat(requestBody).toString();
+    myobj = { name: parsedData};
+  });
+  MongoClient.connect(urll, function(err, db) {
     if (err) throw err;
     var dbo = db.db("test");
-    const requestBody = [];
-    req.on('data', (chunks) => {
-      requestBody.push(chunks);
-    });
-    req.on('end', () => {
-      var parsedData = Buffer.concat(requestBody).toString();
-      var myobj = { name: parsedData};
-      dbo.collection("customers").insertOne(myobj, function (err, res) {
+    dbo.collection("MT").insertOne(myobj, function(err, res) {
       if (err) throw err;
-      console.log("1 document inserted"+ parsedData);
+      console.log("1 document inserted");
       db.close();
-    });
-    });
+   });
   });
+    res.json(req.body);
 });
 app.listen(2020, () => {
-  console.log('server is listening on port 2020');
+    console.log('server is listening on port 2020');
 });
